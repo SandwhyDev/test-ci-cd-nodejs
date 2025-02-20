169bc05e99f5146b76623d1e64df9a439a814815
sandwhydev <sandwhydev@gmail.com>
2025-02-20 11:57:22 +0700

    change github text

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("ci / cd with githuhb actions & deploy");
+  res.send("ci / cd with github actions & deploy");
 });
 
 const PORT = process.env.PORT || 3000;
