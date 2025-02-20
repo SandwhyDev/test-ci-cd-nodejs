819cecfe68470755092bb47431ae33868961fa7c
sandwhydev <sandwhydev@gmail.com>
2025-02-20 11:14:24 +0700

    deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("ci / cd with githuhb actions");
+  res.send("ci / cd with githuhb actions &  sekalian deploy");
 });
 
 const PORT = process.env.PORT || 3000;
