e6c168590b4c2fe3bf215f0879467e8e11219653
sandwhydev <sandwhydev@gmail.com>
2025-02-20 13:09:48 +0700

    up done

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("ci / cd with github actions & deploy");
+  res.send("done deploy");
 });
 
 const PORT = process.env.PORT || 3000;
