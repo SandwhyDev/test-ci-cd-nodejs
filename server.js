fb46f18e7f084516da8a345103458ce740020197
SandwhyDev <sandwhydev@gmail.com>
2025-02-20 10:08:18 +0700

    edit 1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello CI/CD with Node.js! ini");
+  res.send("Hello CI/CD with Node.js! ini edited");
 });
 
 const PORT = process.env.PORT || 3000;
