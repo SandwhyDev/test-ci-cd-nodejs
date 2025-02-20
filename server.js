d3a4f38b894bfb0392fcf663122f5c446d91ff7c
sandwhydev <sandwhydev@gmail.com>
2025-02-20 10:58:58 +0700

    edit text

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello CI/CD with Node.js! ini edited");
+  res.send("ci / cd with githuhb actions");
 });
 
 const PORT = process.env.PORT || 3000;
