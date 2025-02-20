b8b6a29079aca4682bce2c2f376b48e638a1d514
sandwhydev <sandwhydev@gmail.com>
2025-02-20 11:44:05 +0700

    add runners

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("ci / cd with githuhb actions &  sekalian deploy");
+  res.send("ci / cd with githuhb actions & deploy");
 });
 
 const PORT = process.env.PORT || 3000;
