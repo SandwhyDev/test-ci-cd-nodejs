3b87a680449c6b26138e415cd2d8f226b44b2cbd
sandwhydev <sandwhydev@gmail.com>
2025-02-20 13:34:34 +0700

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("done deploy");
+  res.send("hapus secret docker password");
 });
 
 const PORT = process.env.PORT || 3000;
